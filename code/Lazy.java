6f29502cd97247801fcbf4389f760020a47ae4f7
chenjin <chenjin@kingsoft.com>
2019-01-14 08:58:50 +0800

    v1.0

--- code/Lazy.java
+++ code/Lazy.java
@@ -1,10 +1,14 @@
 import java.util.function.Supplier;
 
 public class Lazy {
-
+    public static int compute(int arg) {
+        return arg + 1;
+    }
     public static void main(String [] args) {
-        Integer v1 = 42; // eager
-        Supplier<Integer> v2 = () -> 42; // lazy
+        int v1 = compute(42); // eager
+        System.out.println(v1);
+        Supplier<Integer> v2 = () -> compute(42); // lazy
+        System.out.println(v2);
         System.out.println(v2.get());
     }
 }
